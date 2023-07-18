729af8629e2a298f1145adf4ee8b401c9d3935e3
youranroh <109929835+youranroh@users.noreply.github.com>
2023-07-18 15:28:10 -0400

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,70 @@
+# download the randomwords pip from below to run the Hangman game 
+# https://pypi.org/project/Random-Word/
+
+
+#import the package
+from random_word import RandomWords
+r = RandomWords()
+
+# methods to use
+def find(s, ch):
+    return [i for i, ltr in enumerate(s) if ltr == ch]
+
+# Create a random word
+word = r.get_random_word()
+print(word)
+
+# create a life. This is the hang man
+life = 7
+
+# creat an array that will hold the characters
+answer = []
+usedChars = []
+
+#print("Guessing chances remaining: " + str(life))
+
+for underbars in word:
+    answer.append("_")
+# for loop
+
+while True:
+    if life > 0 and "_" in answer:
+        # print the current status of the answers
+        print('')
+        print(' '.join(map(str, answer))) 
+        print("Guessing chances remaining: " + str(life))
+        
+        # ask the user for an input
+        userInput = input("Guess a character! ")
+        
+        
+        
+        if len(userInput) == 1: # the user input must be a one letter character   
+            if userInput in usedChars:
+                print("\n\nThe character is already used.")
+            # -------- if statement
+            else:
+                if userInput in word:   # if the input is in the word,
+                    index = find(word, userInput)   # get the index of the char in the word
+                    for i in index:
+                        answer[i] = userInput   # and fill in the answer
+                    usedChars.append(userInput)
+                    # ------- if statement
+                else:   # if the input is not contained in the String,
+                    print("\n\nThe character " + userInput + " does not exist in this word! You lose a life!")
+                    life-=1     # deduct a life.
+                    usedChars.append(userInput)
+                # ------- else statement
+        else :
+            print("Invalid input! Try again.")
+        # -------- else statement
+    elif "_" not in answer:
+        print("You win!!")
+        break
+    # --------- elif
+    else:
+        print("You lose!!")
+        break
+    # --------- elif
+
+print("The word was: " + word)
