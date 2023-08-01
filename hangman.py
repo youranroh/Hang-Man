26cf0068c66ba55735ddf893850d9ef5fa0a3632
Youran Roh <youranroh@gmail.com>
2023-08-01 15:18:38 -0400

    I have added visual representation for the hangman

--- hangman.py
+++ hangman.py
@@ -12,10 +12,10 @@ def find(s, ch):
 
 # Create a random word
 word = r.get_random_word()
-print(word)
+#print(word)
 
 # create a life. This is the hang man
-life = 7
+life = 6
 
 # creat an array that will hold the characters
 answer = []
@@ -27,6 +27,17 @@ for underbars in word:
     answer.append("_")
 # for loop
 
+hangman = []
+hangman.append('_________')
+hangman.append('|       |')      # hangman[1]
+hangman.append('|')       # hangman[2]
+hangman.append('|')       # hangman[3]
+hangman.append('|')       # hangman[4]
+hangman.append('|')       # hangman[5]
+hangman.append('|')       
+
+print('\n'.join(hangman))
+
 while True:
     if life > 0 and "_" in answer:
         # print the current status of the answers
@@ -35,9 +46,7 @@ while True:
         print("Guessing chances remaining: " + str(life))
         
         # ask the user for an input
-        userInput = input("Guess a character! ")
-        
-        
+        userInput = input("Guess a character!\n>>> ")
         
         if len(userInput) == 1: # the user input must be a one letter character   
             if userInput in usedChars:
@@ -51,13 +60,27 @@ while True:
                     usedChars.append(userInput)
                     # ------- if statement
                 else:   # if the input is not contained in the String,
-                    print("\n\nThe character " + userInput + " does not exist in this word! You lose a life!")
+                    print("\n\nThe character " + userInput + " does not exist in this word.")
                     life-=1     # deduct a life.
                     usedChars.append(userInput)
+                    if life == 5:
+                        hangman[2] = '|       O'
+                    elif life == 4:
+                        hangman[3] = '|       |'
+                        hangman[4] = '|       |'
+                    elif life == 3:
+                        hangman[3] = '|      \\|'
+                    elif life == 2:
+                        hangman[3] = '|      \\|/'
+                    elif life == 1:
+                        hangman[5] = '|      /'
+                    elif life == 0:
+                        hangman[5] = '|      / \\'    
                 # ------- else statement
         else :
             print("Invalid input! Try again.")
         # -------- else statement
+        print('\n'.join(hangman))
     elif "_" not in answer:
         print("You win!!")
         break
